d63f1ebc42394ffa2edb2d40461044f3268c940e
Soarinferret <thecodye@kanto.cloud>
2020-04-13 21:49:48 -0500

    ./profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -3,7 +3,7 @@
 #
 #
 #    Changelog:
-#        20/04/11 - Begin redesign 
+#        20/04/11 - v2 - Begin redesign 
 #                    remove legacy items
 #                    move some code into separate module
 #                    update prompt
@@ -76,7 +76,7 @@ Param(
 $ProgressPreference='SilentlyContinue'
 $PSProfileVersion = "Remote"
 if(!$Remote){
-    $PSProfileVersion = "1.4." + ((Get-Content $script:MyInvocation.MyCommand.Path | Select-String "/")[0].ToString().Split('-')[0] -replace '\D+(\d+)','$1')
+    $PSProfileVersion = "2.0." + ((Get-Content $script:MyInvocation.MyCommand.Path | Select-String "/")[0].ToString().Split('-')[0] -replace '\D+(\d+)','$1')
 }
 
 #Print Profile Version & Exit
@@ -149,9 +149,9 @@ function Add-PrivateRepo {
         $PublishLocation = "https://packages.kanto.cloud/repository/PowerShell/"
     )
 
-    Register-PSRepository -Name KantoRepo `
-                          -SourceLocation https://packages.kanto.cloud/repository/PowerShell/ `
-                          -PublishLocation https://packages.kanto.cloud/repository/PowerShell/ `
+    Register-PSRepository -Name $Name `
+                          -SourceLocation $SourceLocation `
+                          -PublishLocation $PublishLocation `
                           -PackageManagementProvider nuget `
                           -InstallationPolicy Trusted `
                           -Credential (Get-Credential)
@@ -159,21 +159,6 @@ function Add-PrivateRepo {
 
 # Get-Time
 function Get-Time {  return $(Get-Date).ToLongTimeString() }
-
-# Get-HyperVHost
-Function Get-HyperVHost {
-    Param(
-        [String]$ComputerName = $env:COMPUTERNAME,
-        [PSCredential]$Credential
-    )
-    if ($Credential){
-        $PSDefaultParameterValues = $PSDefaultParameterValues.clone()
-        $PSDefaultParameterValues['*:Credential'] = $Credential
-    }
-    Invoke-command -ComputerName $ComputerName -ScriptBlock {
-        return $(get-item "HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters").GetValue("HostName") 
-    }
-}
 
 # update profile & modules
 function Update-Profile {
@@ -183,32 +168,33 @@ function Update-Profile {
         [String]$Path,
         [Parameter(ParameterSetName='Remote')]
         [string]$URI = "https://raw.githubusercontent.com/SoarinFerret/Pwsh-Profile/master/profile.ps1",
-        [switch]$IncludeModules
+        [String]$ProfilePath = $profile.CurrentUserAllHosts
     )
     # Copy from local location
     if($Path){
         if(Test-Path $Path){
             $confirm = Read-Host "This will overwrite the existing profile. Are you sure you want to proceed? (y/n)"
             if ($confirm -like "y*") {
-                Copy-Item $Path -Destination "$ProfilePath\profile.ps1" -Force
+                Copy-Item $Path -Destination $ProfilePath -Force
             }
         }
     }
     else {
-        Invoke-WebRequest -Uri $URI -OutFile "$ProfilePath\profile.ps1"
+        Invoke-WebRequest -Uri $URI -OutFile $ProfilePath
         # Need to unblock file for Windows hosts
         if($PSEdition -eq "Desktop" -or $PSVersionTable.OS -like "*Windows*"){
-            Unblock-File "$ProfilePath\profile.ps1"
+            Unblock-File "$ProfilePath"
         }
-    }
-    if($IncludeModules){
-        $updateCommand = "$ProfilePath\profile.ps1 -Update"
-        Invoke-Expression $updateCommand
     }
 }
 
 # get profile version
-function Get-ProfileVersion { invoke-expression "$ProfilePath\profile.ps1 -Version" }
+function Get-ProfileVersion {
+    Param(
+        [String]$ProfilePath = $profile.CurrentUserAllHosts
+    ) 
+    invoke-expression "$ProfilePath -Version" 
+}
 
 # why goat farming is better than IT
 Function Get-Goat {
@@ -226,32 +212,6 @@ Function Get-Goat {
     $ProgressPreference = $OriginalPref
 }
 
-# Create Bsod
-function Invoke-Bsod{
-    Param(
-        [String]$Computername = $env:COMPUTERNAME,
-        [Pscredential]$Credential
-    )
-    Write-Host "This will cause a Blue Screen of Death on $Computername.`nAre you sure absolutely sure you want to proceed? (y/n): " -ForegroundColor Red -NoNewline
-    $confirm = Read-Host 
-    if ($confirm -notlike "y*") {
-        return 0;
-    }
-
-    # splat invoke-command
-    $params = @{}
-    if ($computername -notlike $env:COMPUTERNAME -and `
-        $ComputerName -notlike "localhost"){
-        $params['ComputerName'] = $ComputerName
-    }
-    if ($Credential){ $params['Credential'] = $Credential }
-
-    Invoke-Command @params -ScriptBlock {
-        wmic process where processid!=0 call terminate
-    }
-
-}
-
 Function Get-ExternalIPAddress{
     #stolen from https://gallery.technet.microsoft.com/scriptcenter/Get-ExternalPublic-IP-c1b601bb
     Param(
@@ -259,72 +219,6 @@ Function Get-ExternalIPAddress{
     )
     if($full) {return Invoke-RestMethod http://ipinfo.io/json}
     else{return (Invoke-RestMethod http://ipinfo.io/json | Select-object -exp ip)}
-}
-
-# Useful on older versions of powershell
-function Test-Admin {
-    $admin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
-    if (!($admin)){
-        throw "You are not running as an administrator"
-    }
-    else {
-        Write-Verbose "Got admin"
-        return $true
-    }
-}
-
-# Checks windows installer for what version of windows it contains
-function Get-WindowsInstaller {
-    param (
-        [Parameter(Position=0,Mandatory=$true)][String]$DriveLetter
-    )
-    Test-Admin
-    if(!(Get-Volume $DriveLetter[0] -ErrorAction SilentlyContinue)){throw "Volume with the property 'DriveLetter' equal to '$($DriveLetter[0])' cannot be found"}
-    $file = "install.wim"
-    if(Test-Path "$($DriveLetter[0]):\sources\install.esd"){ $file = "install.esd"}
-    for($index = 1; $index -ne 0; $index++){
-        $a = dism /Get-WimInfo /WimFile:$($DriveLetter[0])`:\sources\$file /index:$index | Select-String -Pattern "Name" -SimpleMatch
-        
-        if($a -ne $null){ write-host $a.ToString().SubString(7) }
-        else { $index = -1 }
-    }
-}
-
-# stolen from https://gallery.technet.microsoft.com/scriptcenter/Send-WOL-packet-using-0638be7b
-function Send-WakeOnLan {
-<# 
-  .SYNOPSIS  
-    Send a WOL packet to a broadcast address
-  .PARAMETER mac
-   The MAC address of the device that need to wake up
-  .PARAMETER ip
-   The IP address where the WOL packet will be sent to
-  .EXAMPLE 
-   Send-WOL -mac 00:11:32:21:2D:11 -ip 192.168.8.255 
-#>
-
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory=$True,Position=1)]
-        [string]$MAC,
-        [string]$IP="255.255.255.255", 
-        [int]$Port=9
-    )   
-    $broadcast = [Net.IPAddress]::Parse($ip)
-    $mac=(($mac.replace(":","")).replace("-","")).replace(".","")
-    $target=0,2,4,6,8,10 | ForEach-Object {[convert]::ToByte($mac.substring($_,2),16)}
-    $packet = (,[byte]255 * 6) + ($target * 16)
-    $UDPclient = new-Object System.Net.Sockets.UdpClient
-    $UDPclient.Connect($broadcast,$port)
-    [void]$UDPclient.Send($packet, 102) 
-}
-
-# TODO: add option to send to different computer
-function Invoke-TextToSpeech {
-    param ([Parameter(Mandatory=$true, ValueFromPipeline=$true)] [string] $Text)
-    [Reflection.Assembly]::LoadWithPartialName('System.Speech') | Out-Null   
-    $object = New-Object System.Speech.Synthesis.SpeechSynthesizer 
-    $object.Speak($Text) 
 }
 
 # Get-XKCDPassword 2.0
@@ -356,27 +250,6 @@ function Get-XKCDPassword {
         $out += $pwd
     }
     return $out
-}
-
-function Enable-RemoteDesktop {
-    Param(
-        [String]$ComputerName = $env:COMPUTERNAME,
-        [PSCredential]$Credential
-    )
-    # splat the computername and credential. 'Invoke-Command' is
-    # much quicker if computername is not specified on localhost
-
-    $credHash = @{}
-    if ($computername -notlike $env:COMPUTERNAME -and `
-        $ComputerName -notlike "localhost"){
-        $credHash['ComputerName'] = $ComputerName
-    }
-    if ($Credential){ $credHash['Credential'] = $Credential }
-
-    Invoke-Command @credhash -ScriptBlock{
-        Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0;
-        Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-    }
 }
 
 # credit where credit is due: https://raw.githubusercontent.com/wazuh/wazuh-api/3.9/examples/api-register-agent.ps1
@@ -433,120 +306,8 @@ function Enter-EnhancedPSSession{
     }
 }
 
-#############################################################################################################
-#
-#                                        Modern Authentication O365
-#
-#############################################################################################################
-
-# connect to exchangeonline using modern authentication or basic
-function Connect-ExchangeOnline {
-    Param(
-        [String]$UserPrincipalName = "",
-        [PSCredential]$Credential = $null,
-        [String]$ConnectionURI = 'https://outlook.office365.com/PowerShell-LiveId',
-        [switch]$UseBasic
-    )
-    $PSSession = $null
-
-    # Check if Exchange Online PowerShell module is installed, otherwise revert to old way
-    $Module = "Microsoft.Exchange.Management.ExoPowershellModule.dll"
-    if(!$UseBasic -and ($ModulePath = (Get-ChildItem $env:LOCALAPPDATA\Apps -Recurse -ErrorAction SilentlyContinue | Where-Object {$_.Name -like $Module -and $_.DirectoryName -like "*tion*"}))){
-        $ModulePath= $ModulePath[0].FullName
-        $global:ConnectionUri = $ConnectionUri
-        $global:AzureADAuthorizationEndpointUri = 'https://login.windows.net/common'
-        $global:UserPrincipalName = $UserPrincipalName
-        Import-Module $ModulePath
-        $PSSession = New-ExoPSSession -UserPrincipalName $UserPrincipalName -ConnectionUri $ConnectionUri -AzureADAuthorizationEndpointUri $AzureADAuthorizationEndpointUri
-    }
-    else{
-        $PSSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $ConnectionURI -AllowRedirection -Credential $Credential -Authentication Basic
-    }
-    if ($PSSession -ne $null) { Import-PSSession $PSSession -AllowClobber }
-}
-
-# connect to the security and compliance center using modern or basic authentication
-function Connect-SecurityAndComplianceCenter {
-    Param(
-        $UserPrincipalName = "",
-        [PSCredential]$Credential = $null,
-        $ConnectionURI = 'https://ps.compliance.protection.outlook.com/PowerShell-LiveId',
-        [switch]$UseBasic
-    )
-    $param = @{UserPrincipalName=$UserPrincipalName;Credential=$Credential;ConnectionURI=$ConnectionURI;UseBasic=$UseBasic}
-    Connect-ExchangeOnline @param
-}
-
-#############################################################################################################
-#
-#                                             Hosts File
-#
-#############################################################################################################
-
-# TODO: Remove-HostsFile
-
-function Search-HostsFile {
-    Param(
-        [String]$Hostname = "*",
-        [ipaddress]$IP = $null
-    )
-    $file = ""
-    if($PSEdition -eq "Desktop" -or $PSVersionTable.OS -like "*Windows*"){
-        $file = "$env:windir\System32\drivers\etc\hosts"
-    } else { $file = "/etc/hosts" }
-    $lines = Get-Content $file | Where-Object {$_[0] -ne '#' -and $_.trim() -ne "" -and $_ -like $Hostname}
-    if($ipaddress -ne $null){
-        $lines = $lines | Where-Object {$_ -like $ipaddress}
-    }
-    $hosts = @()
-    forEach ($line in $lines){
-        $parts = $line -replace "#.*" -split '\s+' # doesnt include EOL comments like this
-        $ip = $parts[0]
-        $names = $parts[1..($parts.Length-1)] | Where-Object {$_ -ne ""}
-        $hosts += New-Object -TypeName psobject -Property @{IPAddress=$ip;Hostname=$names}
-    }
-    return $hosts
-}
-
-function Add-HostsFile {
-    Param(
-        [Parameter(Mandatory=$true)]
-        [String[]]$Hostname,
-        [Parameter(Mandatory=$true)]
-        [ipaddress]$IP
-    )
-    Test-Admin
-    $file = ""
-    if($PSEdition -eq "Desktop" -or $PSVersionTable.OS -like "*Windows*"){
-        $file = "$env:windir\System32\drivers\etc\hosts"
-    } else { $file = "/etc/hosts" }
-    "$($ip.IPAddressToString)`t$hostname" | Out-File $file -Append -Encoding ascii
-}
-
-function Open-HostsFile {
-    Start-Process notepad "$env:windir\System32\drivers\etc\hosts"
-}
-
-#############################################################################################################
-#
-#                                   Get Cool Modules from Me/PSGallery
-#
-#############################################################################################################
-
-function profileGetModules{ 
-    #install NuGet
-    if(!(Get-Module -ListAvailable -Name PackageManagement)){
-        Get-PackageProvider -Name NuGet -Force | Out-Null
-    }
-
-    $modules = "NTFSSecurity","Posh-SSH","AzureAD"
-
-    ForEach($module in $modules){
-        if(!(Get-Module -ListAvailable -Name $module)){
-            Find-Module $module
-            Install-Module $module -Force -AllowClobber
-        }else {Update-Module $module}
-    }
+function Set-LocationToHome{
+    Set-Location ~/
 }
 
 #############################################################################################################
@@ -560,7 +321,10 @@ function profileSetAlias{
         [parameter(Position=0)][String]$Alias,
         [parameter(Position=1)][String]$Command
     )
-    if (!(Get-alias $Alias -ErrorAction SilentlyContinue) -and (Get-Command $Command -ErrorAction SilentlyContinue)){
+    if(!(Get-alias $Alias -ErrorAction SilentlyContinue) -and `     # check alias doesn't exist
+        !(Get-Command $alias -ErrorAction SilentlyContinue) -and `  # check alias isn't already a command
+         (Get-Command $Command -ErrorAction SilentlyContinue)       # check command exists
+      ){ 
         new-Alias $Alias $Command -Scope 1
     }
 }
@@ -568,8 +332,6 @@ function profileSetAlias{
 # Standard Cmdlets
 profileSetAlias touch New-Item
 profileSetAlias grep Select-String
-profileSetAlias get-commands get-command #bc I always accidently type this instead
-profileSetAlias gcmd get-command
 profileSetAlias Shutdown-Computer Stop-Computer #because it makes more sense
 
 # PS Core Aliases
@@ -586,17 +348,11 @@ profileSetAlias Reset-ADAccountPassword Set-ADAccountPassword #because I cant re
 profileSetAlias gt Get-Time
 profileSetAlias gg Get-Goat
 profileSetAlias geip Get-ExternalIPAddress
-profileSetAlias Test-isAdmin Test-Admin
-profileSetAlias Check-WindowsInstaller Get-WindowsInstaller
-profileSetAlias Send-WOL Send-WakeOnLan
 profileSetAlias gxp Get-XKCDPassword
-profileSetAlias Enable-RDP Enable-RemoteDesktop
-profileSetAlias eps Enter-EnhancedPSSession
+profileSetAlias eps Enter-PSSession
+profileSetAlias eeps Enter-EnhancedPSSession
 profileSetAlias Ignore-SelfSignedCerts Use-SelfSignedCerts
-
-# O365 Modern Auth
-profileSetAlias Connect-Exo Connect-ExchangeOnline
-profileSetAlias Connect-SaCC Connect-SecurityAndComplianceCenter
+profileSetAlias gh Set-LocationToHome
 
 #############################################################################################################
 #
@@ -625,6 +381,3 @@ $ProgressPreference='Continue'
 # Clean up items
 Remove-Item -Path Function:\profile*
 Remove-Variable Update,Version
-
-# Change Directory to $home
-Set-Location $home
